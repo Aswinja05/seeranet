12d30a556328d41bd07bd7aa59bbe0c47b50eda8
Aswinja05 <asja22ise@cmrit.ac.in>
2025-04-28 18:18:23 +0530

    added sitemap

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // "mongodb+srv://AdminNew:Aswin123@cluster0.phbtcqz.mongodb.net/Seera?retryWrites=true&w=majority"
 // Connect to MongoDB
 mongoose
-  .connect("mongodb+srv://AdminNew:Aswin123@cluster0.phbtcqz.mongodb.net/Seera?retryWrites=true&w=majority", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
